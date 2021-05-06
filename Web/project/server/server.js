81bca1bbfdfe5318cc1a1616e314616438d17469
SKrawczyk <61988998+SzymonKrawczyk@users.noreply.github.com>
2021-05-06 16:17:47 +0200

    tcp server time

--- Web/project/server/server.js
+++ Web/project/server/server.js
@@ -45,31 +45,30 @@ app.listen(process.env.SERVER_PORT, () =>
 	
 	
 	
-	/*// test
+	
+	
+	
+	
+	// zegar usługa free
 	var net = require('net');
 	var server = net.createServer();    
 	server.on('connection', handleConnection);
 	server.listen(8052, function() {    
 	  console.log('tcp server listening to %j', server.address());  
 	});
-	function handleConnection(conn) {    
-	  var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
-	  console.log('new client connection from %s', remoteAddress);
-	  conn.setEncoding('utf8');
-	  conn.on('data', onConnData);  
-	  conn.once('close', onConnClose);  
-	  conn.on('error', onConnError);
-	  function onConnData(d) {  
-		console.log('connection data from %s: %j', remoteAddress, d);  
-		conn.write(d.toUpperCase());  
-	  }
-	  function onConnClose() {  
-		console.log('connection from %s closed', remoteAddress);  
-	  }
-	  function onConnError(err) {  
-		console.log('Connection %s error: %s', remoteAddress, err.message);  
-	  }  
-	}*/
+	function handleConnection(conn) {  
+	
+		var remoteAddress = conn.remoteAddress + ':' + conn.remotePort;  
+		console.log('new client connection from %s', remoteAddress);
+		conn.setEncoding('utf8');
+		
+		const timestamp = (new Date()).getTime();
+		console.log(`Sending date... ${timestamp} = ${new Date(timestamp)}`);
+	  	  
+		conn.write("" + timestamp); 
+
+		conn.destroy();
+	}
 })
 
 
